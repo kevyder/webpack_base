8081182563ace22e18e2aeb8566f2adf108c7a59
Kevin <kyrodriguez97@gmail.com>
2020-05-30 16:57:44 -0500

    Install postcss

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,39 @@ module.exports = {
   },
 
   mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.js$/,
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+
+        use: [
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
