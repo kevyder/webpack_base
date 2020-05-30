93fbde6ce2be62784ed8056219d270dd1b5f8f95
Kevin <kyrodriguez97@gmail.com>
2020-05-30 15:56:48 -0500

    Bootstraping

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/javascript/index.js',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+
+  mode: 'development'
+};
